2346d4463334af882adc20cef4e8b2fa57a0ecc0
singh-deep-raman <raman111092@gmail.com>
2020-09-20 16:36:25 +0530

    index page added. lesson no. 100

---
+++ src/main/java/com/raman/recipe/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.raman.recipe.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","/index","/index.html"})
+    public String indexPage() {
+        return "index";
+    }
+}
